ac76013acc2ba6eb868281a83037f619535eb9a0
Swathi <spanchan@andrew.cmu.edu>
2017-05-01 11:35:28 -0400

    added borrow item details screen layout

--- app/src/main/java/com/exchange/crowd/crowdexchange/BorrowItemDetailsActivity.java
+++ app/src/main/java/com/exchange/crowd/crowdexchange/BorrowItemDetailsActivity.java
@@ -1,46 +1,34 @@
 package com.exchange.crowd.crowdexchange;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class  MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button b1;
-    Button b2;
+public class BorrowItemDetailsActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        b1 = (Button) findViewById(R.id.button1);
+        setContentView(R.layout.activity_borrow_item_details);
+        Button b1 = (Button) findViewById(R.id.chat_now);
         b1.setOnClickListener(this);
-
-        b2 =(Button) findViewById(R.id.button2);
-        b2.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View v)
-    {
+    public void onClick(View v) {
         // TODO Auto-generated method stub
         switch(v.getId()){
-            case R.id.button1:
+            case R.id.chat_now:
 
                 //Replace this to call my layout
                 setContentView(R.layout.activity_login);
                 Intent i = new Intent(this, LoginActivity.class);
                 startActivity(i);
                 break;
-            case R.id.button2:
-                setContentView(R.layout.activity_signup_guider);
-                Intent p = new Intent(this, signupGuider.class);
-                startActivity(p);
-                break;
+
         }
-
     }
-
 }
